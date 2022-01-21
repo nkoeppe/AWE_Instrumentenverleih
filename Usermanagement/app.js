fa35ef26ed1cd81f662fccb9af8dd1d207ff8daf
Nico Mac <n.koeppe@gmx.de>
2022-01-21 13:33:18 +0100

    usermgmt + authservice

--- Usermanagement/app.js
+++ Usermanagement/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -16,28 +17,29 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
-	extended: false
+  extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/authService', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.send('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
 });
 
 module.exports = app;
--- Usermanagement/src/userService.js
+++ Usermanagement/src/userService.js
@@ -6,271 +6,271 @@ var mysql = require('mysql2/promise');
 let client;
 
 class UserService {
-	constructor() {
+  constructor() {
 
-	}
+  }
 
-	async connectDatabase() {
-		client = await mysql.createConnection({
-			host: '127.0.0.1',
-			user: 'root',
-			password: '',
-			database: 'userdata',
-			port: 3306
-		});
-		//
-		// client.connect(function(err) {
-		// 	if (err) console.log(err);
-		// 	console.log('Connected!');
-		// });
-	}
+  async connectDatabase() {
+    client = await mysql.createConnection({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'usermanagement',
+      port: 3306
+    });
+    //
+    // client.connect(function(err) {
+    // 	if (err) console.log(err);
+    // 	console.log('Connected!');
+    // });
+  }
 
-	async disconnectDatabase() {
-		client.end();
-	}
-	async default () {
-		try {
-			if (true) return `API up an running ${client}`;
+  async disconnectDatabase() {
+    client.end();
+  }
+  async default () {
+    try {
+      if (true) return `API up an running ${client}`;
 
-		} catch (e) {
-			return e;
-		}
-	}
+    } catch (e) {
+      return e;
+    }
+  }
 
-	async functionWrapper(next, args) {
-		try {
-			await this.connectDatabase();
-			const result = await next(args);
-			await this.disconnectDatabase();
-			return result;
+  async functionWrapper(next, args) {
+    try {
+      await this.connectDatabase();
+      const result = await next(args);
+      await this.disconnectDatabase();
+      return result;
 
-		} catch (e) {
+    } catch (e) {
 
-			await this.disconnectDatabase();
-			throw e;
-		} finally {
+      await this.disconnectDatabase();
+      throw e;
+    } finally {
 
-		}
-	}
+    }
+  }
 
-	async getAllUser({
-		that
-	}) {
-		try {
-			var sql = 'SELECT * from user';
-			let [rows] = await client.execute(sql);
-			rows = await Promise.all(rows.map(async (row) => {
+  async getAllUser({
+    that
+  }) {
+    try {
+      var sql = 'SELECT * from user';
+      let [rows] = await client.execute(sql);
+      // rows = await Promise.all(rows.map(async (row) => {
+      //
+      //   row['roles'] = await that.getRoleForUser({
+      //     id: row.ID,
+      //     that: that
+      //   });
+      //   return row;
+      // }));
+      return {
+        rows
+      };
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
 
-				row['roles'] = await that.getRoleForUser({
-					id: row.ID,
-					that: that
-				});
-				return row;
-			}));
-			return {
-				rows
-			};
-		} catch (e) {
-			console.log(e);
-			throw e;
-		}
-	}
+  async getSingleUser({
+    id,
+    that
+  }) {
+    try {
+      var sql = `SELECT * from user WHERE ID = ${id}`;
+      let [rows] = await client.execute(sql);
+      // rows = await Promise.all(rows.map(async (row) => {
+      //
+      //   row['roles'] = await that.getRoleForUser({
+      //     id: row.ID,
+      //     that: that
+      //   });
+      //   return row;
+      // }));
+      if (true) return {
+        rows
+      };
 
-	async getSingleUser({
-		id,
-		that
-	}) {
-		try {
-			var sql = `SELECT * from user WHERE ID = ${id}`;
-			let [rows] = await client.execute(sql);
-			rows = await Promise.all(rows.map(async (row) => {
-
-				row['roles'] = await that.getRoleForUser({
-					id: row.ID,
-					that: that
-				});
-				return row;
-			}));
-			if (true) return {
-				rows
-			};
-
-		} catch (e) {
-			console.log(e);
-			return e;
-		}
-	}
-
-
-	async createUser({
-		username,
-		password,
-		roleID = 1, //Default Role: Guest
-		that
-	}) {
-		try {
-			const sqlUser = `INSERT INTO user (username, password) VALUES ('${username}', '${password}')`;
-
-			let result = await client.execute(sqlUser);
-			const userID = result[0].insertId;
-			const sqlUserRole = `INSERT INTO user_roles (user_id, role_id) VALUES ('${userID}','${roleID}')`;
-
-			result = await client.execute(sqlUserRole);
-			return {
-				success: true
-			};
-
-		} catch (e) {
-			console.log(e);
-			return {
-				success: false,
-				error: e
-			};
-		}
-	}
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
+  }
 
 
-	async getRoleForUser({
-		id,
-		that
-	}) {
-		try {
-			var sql = `SELECT * from user_roles WHERE user_id = ${id}`;
-			let [rows] = await client.execute(sql);
-			rows = await Promise.all(rows.map(async (item) => {
-				const role = await that.getSingleRole({
-					id: item.role_id,
-					that: that
-				});
-				return role;
-			}));
-			return rows;
+  async createUser({
+    username,
+    password,
+    roleID = 1, //Default Role: Guest
+    that
+  }) {
+    try {
+      const sqlUser = `INSERT INTO user (username, password) VALUES ('${username}', '${password}')`;
 
-		} catch (e) {
-			console.log(e);
-			throw e;
-		}
-	}
+      let result = await client.execute(sqlUser);
+      const userID = result[0].insertId;
+      const sqlUserRole = `INSERT INTO user_roles (user_id, role_id) VALUES ('${userID}','${roleID}')`;
 
-	async getPermissionsForRole({
-		id,
-		that
-	}) {
-		try {
+      result = await client.execute(sqlUserRole);
+      return {
+        success: true
+      };
 
-			// console.log(id);
-			var sql = `SELECT * from role_permissions WHERE role_id = ${id}`;
-			// console.log(sql);
-			let [rows] = await client.execute(sql);
-			rows = await Promise.all(rows.map(async (item) => {
-				return (({
-					name,
-					...description
-				}) => (
-					name
-				))(await that.getSinglePermission({
-					id: item.permission_id
-				}));
-			}));
-			// console.log(rows);
-			// console.log(rows);
-			return rows;
-
-		} catch (e) {
-			console.log(e);
-			throw e;
-		}
-	}
-
-	async getAllRoles({
-		that
-	}) {
-		try {
-			var sql = 'SELECT * from role';
-			let [rows] = await client.execute(sql);
-			rows = await Promise.all(rows.map(async (row) => {
-
-				const t = await that.getPermissionsForRole({
-					id: row.ID,
-					that: that
-				});
-				row['permissions'] = t;
-				console.log(row);
-				return row;
-			}));
-			return rows;
-
-		} catch (e) {
-			console.log(e);
-			throw e;
-		}
-	}
+    } catch (e) {
+      console.log(e);
+      return {
+        success: false,
+        error: e
+      };
+    }
+  }
 
 
-	async getSingleRole({
-		id,
-		that
-	}) {
-		try {
-			var sql = `SELECT * from role WHERE ID = ${id}`;
-			let [rows] = await client.execute(sql);
-			rows = await Promise.all(rows.map(async (row) => {
-				const t = await that.getPermissionsForRole({
-					id: row.ID,
-					that: that
-				});
-				row['permissions'] = t;
-				return row;
-			}));
-			return rows;
+  async getRoleForUser({
+    id,
+    that
+  }) {
+    try {
+      var sql = `SELECT * from user_roles WHERE user_id = ${id}`;
+      let [rows] = await client.execute(sql);
+      rows = await Promise.all(rows.map(async (item) => {
+        const role = await that.getSingleRole({
+          id: item.role_id,
+          that: that
+        });
+        return role;
+      }));
+      return rows;
+
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  async getPermissionsForRole({
+    id,
+    that
+  }) {
+    try {
+
+      // console.log(id);
+      var sql = `SELECT * from role_permissions WHERE role_id = ${id}`;
+      // console.log(sql);
+      let [rows] = await client.execute(sql);
+      rows = await Promise.all(rows.map(async (item) => {
+        return (({
+          name,
+          ...description
+        }) => (
+          name
+        ))(await that.getSinglePermission({
+          id: item.permission_id
+        }));
+      }));
+      // console.log(rows);
+      // console.log(rows);
+      return rows;
+
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  async getAllRoles({
+    that
+  }) {
+    try {
+      var sql = 'SELECT * from role';
+      let [rows] = await client.execute(sql);
+      rows = await Promise.all(rows.map(async (row) => {
+
+        const t = await that.getPermissionsForRole({
+          id: row.ID,
+          that: that
+        });
+        row['permissions'] = t;
+        console.log(row);
+        return row;
+      }));
+      return rows;
+
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
 
 
-		} catch (e) {
-			console.log(e);
-			return e;
-		}
-	}
-
-	async getAllRolePermissions() {
-		try {
-			var sql = 'SELECT * from role_permissions';
-			let [rows] = await client.execute(sql);
-			return {
-				rows
-			};
-		} catch (e) {
-			console.log(e);
-			throw e;
-		}
-	}
+  async getSingleRole({
+    id,
+    that
+  }) {
+    try {
+      var sql = `SELECT * from role WHERE ID = ${id}`;
+      let [rows] = await client.execute(sql);
+      rows = await Promise.all(rows.map(async (row) => {
+        const t = await that.getPermissionsForRole({
+          id: row.ID,
+          that: that
+        });
+        row['permissions'] = t;
+        return row;
+      }));
+      return rows;
 
 
-	async getAllPermissions() {
-		try {
-			var sql = 'SELECT * from permissions';
-			let [rows] = await client.execute(sql);
-			return {
-				rows
-			};
-		} catch (e) {
-			console.log(e);
-			throw e;
-		}
-	}
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
+  }
+
+  async getAllRolePermissions() {
+    try {
+      var sql = 'SELECT * from role_permissions';
+      let [rows] = await client.execute(sql);
+      return {
+        rows
+      };
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
 
 
-	async getSinglePermission({
-		id
-	}) {
-		try {
-			let [rows] = await client.execute(`SELECT * from permissions WHERE ID = ${id}`);
+  async getAllPermissions() {
+    try {
+      var sql = 'SELECT * from permissions';
+      let [rows] = await client.execute(sql);
+      return {
+        rows
+      };
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
 
-			return rows[0];
 
-		} catch (e) {
-			return e;
-		}
-	}
+  async getSinglePermission({
+    id
+  }) {
+    try {
+      let [rows] = await client.execute(`SELECT * from permissions WHERE ID = ${id}`);
+
+      return rows[0];
+
+    } catch (e) {
+      return e;
+    }
+  }
 
 
 }
