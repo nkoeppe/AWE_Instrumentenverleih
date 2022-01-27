c843d645498ea742d72760ac5e35bfc761f7e077
Alexander Fitz-Gibbon <alexander.fitz-gibbon@fkie.fraunhofer.de>
2022-01-27 11:50:47 +0100

    Projektdateien hinzufügen.

---
+++ Instrumentenverleih/Models/Hersteller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instrumentenverleih.Models
+{
+    public class Hersteller
+    {
+        private int id;
+        private string name;
+        private int plz;
+        private string ort;
+        private string strasse;
+
+        public string Name { get => name; set => name = value; }
+        public int Plz { get => plz; set => plz = value; }
+        public string Ort { get => ort; set => ort = value; }
+        public string Strasse { get => strasse; set => strasse = value; }
+
+
+        public Hersteller()
+        {
+            id = 0;
+            name = "";
+            plz = 0;
+            ort = "";
+            strasse = "";
+        }
+
+        public Hersteller(int id, string name, int plz, string ort, string strasse)
+        {
+            this.id = id;
+            this.name = name;
+            this.plz = plz;
+            this.ort = ort;
+            this.strasse = strasse;
+        }
+    }
+}
---
+++ Instrumentenverleih/Models/Instrument.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instrumentenverleih.Models
+{
+    public class Instrument
+    {
+        private int id;
+        private string name;
+        private Hersteller hersteller;
+        private double preis;
+        private bool ausgeliehen;
+
+        public string Name { get => name; set => name = value; }
+        public Hersteller Hersteller { get => hersteller; set => hersteller = value; }
+        public double Preis { get => preis; set => preis = value; }
+        public bool Ausgeliehen { get => ausgeliehen; set => ausgeliehen = value; }
+
+        public Instrument()
+        {
+            id = 0;
+            name = "";
+            hersteller = new Hersteller();
+            preis = 0;
+            ausgeliehen = false;
+        }
+
+        public Instrument(int id, string name, Hersteller hersteller, double preis, bool ausgeliehen)
+        {
+            this.id = id;
+            this.name = name;
+            this.hersteller = hersteller;
+            this.preis = preis;
+            this.ausgeliehen = ausgeliehen;
+        }
+    }
+}
