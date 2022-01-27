fb8c34fcb6165bdf2c218954dc63955d16cad7b6
fl <fl@misoft.de>
2022-01-27 11:48:44 +0100

    Projektdateien hinzufügen.

---
+++ Kundenverwaltung_/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Kundenverwaltung_.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Kundenverwaltung_/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Kundenverwaltung_.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
